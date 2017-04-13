f8c7f140c12b7766816b760b60628c0384d90366
LeoAnka <erkbot1@gmail.com>
2017-04-13 14:33:36 +0200

    galfano fix
    parsing fix with BeautifulSoup, to extract only text words from web

--- temp/spiderk_00.py
+++ temp/spiderk_00.py
@@ -13,9 +13,14 @@ from bs4 import BeautifulSoup
 scontext = None
 
 
+# 1. Elegir una página web.
+# 2. Extraer el texto de esa página.
+# 3. Dividirlo en palabras.
+# 4. Quitar las palabras no válidas (números, artículos, etc.).
+# 5. Guardar cada palabra y el número de veces que ha salido.
 
 
-conn = sqlite3.connect("webdata")
+conn = sqlite3.connect("webdata.sqlite")
 cur = conn.cursor()
 
 
@@ -34,9 +39,12 @@ pass # check url is correct and exists
 
 web = urllib.request.urlopen(url).read()
 print (web)
-
 soup = BeautifulSoup(web, "html.parser")
 
+# Quita el texto que haya dentro de las etiquetas <script> (código javascript) y <style> (Estilos CSS).
+# Ninguno de esos textos nos interesa.
+for script in soup(["script", "style"]):
+    script.extract()
 
 
 dataText = soup.get_text()
@@ -47,8 +55,11 @@ print (text)
 
 
 
-
+# Mejor solución: http://stackoverflow.com/questions/1936466/beautifulsoup-grab-visible-webpage-text
 
 #tags = soup.find_all("p")
 #for tag in tags:
     #print (soup.get_text())
+
+# texts = soup.findAll(text=True)
+# print(texts)
