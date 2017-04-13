a04a75930c7c9ed8904fb555235d29232cc34cff
LeoAnka <erkbot1@gmail.com>
2017-04-13 16:51:17 +0200

    cleaning extracted text
    still not working

--- spiderk_00.py
+++ spiderk_00.py
@@ -1,8 +1,10 @@
 
 
+
 import sqlite3
 import urllib.request
 from bs4 import BeautifulSoup
+import re
 #import ssl
 
 
@@ -13,6 +15,11 @@ from bs4 import BeautifulSoup
 scontext = None
 
 
+# 1. Elegir una página web.
+# 2. Extraer el texto de esa página.
+# 3. Dividirlo en palabras.
+# 4. Quitar las palabras no válidas (números, artículos, etc.).
+# 5. Guardar cada palabra y el número de veces que ha salido.
 
 
 conn = sqlite3.connect("webdata.sqlite")
@@ -32,23 +39,47 @@ print ("web target is", url)
 
 pass # check url is correct and exists
 
-web = urllib.request.urlopen(url).read()
-print (web)
+web = urllib.request.urlopen(url).read() #decode.('utf8') probar
 
+print ("01 - TEXTO BRUTO EN BINARIO -----------------------------------------------------------")
+print (web)
 soup = BeautifulSoup(web, "html.parser")
 
+# Quita el texto que haya dentro de las etiquetas <script> (código javascript) y <style> (Estilos CSS).
+# Ninguno de esos textos nos interesa.
+for script in soup(["script", "style"]):
+    script.extract()
 
 
 dataText = soup.get_text()
-text = dataText.strip()
+text = str(dataText)
+print ("02 - TEXTRO EXTRAIDO CON BEAUTIFULSOUP EN STRING ------------------------------------------")
 print (text)
 
+textClean = text.split()
+print ("03 - LISTA DE PALABRAS EXTRAIDAS CON METODO SPLIT--------------------------------------------")
+print (textClean)
+
+
+#words = re.findall(" ([^AB]+) ", dataText)
+#print (words)
+
+#dataText.split()
+#print (dataText)
 
 
 
 
 
+
+
+
+
+# Mejor solución: http://stackoverflow.com/questions/1936466/beautifulsoup-grab-visible-webpage-text
 
 #tags = soup.find_all("p")
 #for tag in tags:
     #print (soup.get_text())
+
+# texts = soup.findAll(text=True)
+# print(texts)
