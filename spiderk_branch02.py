6242666382f9b17697a4f54748b627ed575f4ae0
LeoAnka <erkbot1@gmail.com>
2017-05-28 17:37:44 +0200

    cambios

--- spiderk_branch02.py
+++ spiderk_branch02.py
@@ -12,11 +12,12 @@ import re
 scontext = None
 
 
-# 1. Elegir una página web.
-# 2. Extraer el texto de esa página.
-# 3. Dividirlo en palabras.
-# 4. Quitar las palabras no válidas (números, artículos, etc.).
-# 5. Guardar cada palabra y el número de veces que ha salido.
+# Elegir una página web.
+# Extraer el texto de esa página.
+# Limpiarlo de caracteres no válidos
+# Dividirlo en palabras.
+# Quitar las palabras no válidas (números, artículos, etc.).
+# Guardar cada palabra y el número de veces que ha salido.
 
 
 #------------------------
@@ -41,11 +42,16 @@ conn = sqlite3.connect("webdata.sqlite")
 cur = conn.cursor()
 
 
-#url = "http://www.faz.net/aktuell/wirtschaft/unternehmen/deutsche-bahn-auf-chef-suche-alles-hoert-auf-kein-kommando-14844033.html"
-url = "https://slowgerman.com/"
+url = "http://www.faz.net/aktuell/wirtschaft/unternehmen/deutsche-bahn-auf-chef-suche-alles-hoert-auf-kein-kommando-14844033.html"
+#url = "http://slowgerman.com/"
+#url = "http://elpais.com/"
+
 # check url is correct and exists
 urlcode = urllib.request.urlopen(url).getcode()
-print("urlcode", urlcode) # if code = 200, all is ok
+if urlcode == 200:                        # if code = 200, all is ok
+    print("\nconection success with your web:")
+    print(url)
+    print("\n")
 
 
 web = urllib.request.urlopen(url).read()  #.decode('utf8') to see html tree
@@ -84,6 +90,40 @@ for entry in entries:
         else:
             wordsList.append(word)
 
+"""
+for word in wordsList:
+    print(word)                     #TO SEE EXTRACTED WORDS
+"""
+
+dic = {}
 
 for word in wordsList:
-    print(word)
+    dic[word] = dic.get(word, 0)
+    dic[word] += 1
+
+#print(dic)
+#print(sorted(dic))                 #ORDER BY DIC KEYS
+#print(sorted(dic, key=dic.get))   #ORDER BY DIC VALUES
+
+dicSorted = sorted(dic, key=dic.get, reverse=True)
+#print(dicSorted)                    #dicSorted is a LIST
+
+"""
+for word in dicSorted:
+    print(word, dic[word])         #TO SEE SORTED DICTIONARY
+
+for word in dicSorted[0:11]:       #TO SEE SORTED DICTIONARY enties 1-10
+    print(word, dic[word])
+"""
+
+database = sqlite3.connect("dbwords.sqlite")
+cur = database.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS words (word TEXT UNIQUE, count INTEGER)""")
+
+cur.execute("""CREATE TABLE IF NOT EXISTS links (url TEXT UNIQUE)""")
+
+for word in dicSorted[0:101]:
+    #print(word)
+    cur.execute("INSERT INTO words (word, count) VALUES (?, ?)", (word, dic[word]))
+database.commit()
