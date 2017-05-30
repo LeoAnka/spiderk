0ebb4640cc589bc484dd10519406d4504639df0d
LeoAnka <erkbot1@gmail.com>
2017-05-30 15:19:08 +0200

    improved

---
+++ wordselection01.py
@@ -0,0 +1,47 @@
+import sqlite3
+import re
+
+
+database = sqlite3.connect("dbwords.sqlite")
+cur = database.cursor()
+
+#many = 50           # User defines how many words wants to be displayed
+try:
+    many = int(input("how many words you want to see: "))
+except:
+    many = 50
+    print ("wrong input. Default 50 words will be shown:\n")
+
+filters = ["die", "der", "und", "von"]
+
+#while (many > 0):
+
+allwords = "SELECT word, count FROM words ORDER BY count DESC"
+filterwords = []
+filtered = False
+
+for row in cur.execute(allwords):
+    for word in filters:
+        if row[0] == word:
+            #print("word filtered")
+            filtered = True
+            break
+        else:
+            filtered = False
+            continue
+
+    if filtered == False:
+        filterwords.append((row[0], row[1]))
+        many -= 1
+        #print("many left:", many)
+        if (many < 1):
+            break
+
+
+for row in filterwords:
+    print(row[0], row[1])
+
+"""
+for row in cur.execute("SELECT word, count FROM words ORDER BY count DESC LIMIT ?", (many,)):
+    print(row[0], row[1])
+"""
